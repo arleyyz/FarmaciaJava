8d97db9d55a8cca7bc13794cdabe6f6ab4bfbac3
gabcadi <gcastroa16@gmail.com>
2023-07-23 11:57:15 -0600

    Version 1

---
+++ src/main/java/com/mycompany/farmacia/Farmacia.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.farmacia;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gcastro
+ */
+public class Farmacia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
